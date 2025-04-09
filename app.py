997c50cebb48b1e4589e79587606e3b027cd1f77
Raushan Kumar <147327480+raushan0785@users.noreply.github.com>
2025-04-09 23:02:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, HTMLResponse
 import whisper
 import tempfile
 import os
 
+
+os.environ["XDG_CACHE_HOME"] = "/tmp/.cache"
+
 app = FastAPI()
 
-# Enable CORS for frontend
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,21 +19,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load Whisper model
-model = whisper.load_model("base")
+
+model = whisper.load_model("tiny") 
+
+
+@app.get("/", response_class=HTMLResponse)
+async def serve_index():
+    return FileResponse("index.html")
 
 @app.post("/transcribe/")
 async def transcribe_audio(file: UploadFile = File(...)):
-    # Save uploaded file to a temporary location
+   
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
         temp_audio.write(await file.read())
         temp_audio_path = temp_audio.name
 
-    # Transcribe audio
+
     result = model.transcribe(temp_audio_path)
-    
-    # Delete temp file
+
+
     os.remove(temp_audio_path)
 
     return {"filename": file.filename, "transcription": result["text"]}
-
